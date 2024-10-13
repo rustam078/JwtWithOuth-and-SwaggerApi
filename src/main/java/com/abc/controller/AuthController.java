785bd039a820ecdd4daaea2cb99ccfc999cdee44
Rustam+1-at-963782499227 <Rustam+1-at-963782499227>
2024-10-13 12:06:09 +0530

    Teting

--- src/main/java/com/abc/controller/AuthController.java
+++ src/main/java/com/abc/controller/AuthController.java
@@ -12,6 +12,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,6 +50,12 @@ public class AuthController {
 		UserEntity user = userService.registerUser(request);
 		return ResponseEntity.ok(user);
 	}
+	
+	@GetMapping("/test")
+		public String hello() {
+			return "Rustam";
+		}
+	
 
 	@PostMapping("/signin")
 	public ResponseEntity<?> authenticateUser(@RequestBody AuthenticationRequest loginRequest) {
